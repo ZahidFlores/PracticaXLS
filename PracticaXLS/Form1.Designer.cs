7c3489d9b9274c64b39f4d647d4f3a5d6434595d
Zahid Flores <Zahid Flores@DESKTOP-DDN13PI>
2023-09-11 02:16:11 -0600

    Agregar archivos de proyecto.

---
+++ PracticaXLS/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace PracticaXLS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvReporte = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvReporte
+            // 
+            this.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte.Location = new System.Drawing.Point(36, 43);
+            this.dgvReporte.Name = "dgvReporte";
+            this.dgvReporte.Size = new System.Drawing.Size(461, 341);
+            this.dgvReporte.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2019",
+            "2020"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Año";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(36, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Generar Reporte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 456);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dgvReporte);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvReporte;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PracticaXLS/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace PracticaXLS
+{
+    public partial class Form1 : Form
+    {
+        static string server = "Data Source = DESKTOP-DDN13PI\\SQLEXPRESS; Initial Catalog= Reportes; Integrated Security = True ";
+        SqlConnection conectar = new SqlConnection(server);
+        DataTable dataTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conectar.Open();
+            string consulta = "select * from ventas";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, conectar);
+            adapter.Fill(dataTable);
+            dgvReporte.DataSource = dataTable;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                try
+                {
+                    Excel.Application excelApp = new Excel.Application();
+                    Excel.Workbook workbook = excelApp.Workbooks.Open(@"C:\Users\Zahid Flores\Desktop\Formato Reporte.xlsx");
+                    Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Sheets["Hoja1"];
+                    for (int i = 0; i < dgvReporte.RowCount - 1; i++)
+                    {
+                        for (int j = 0; j < dgvReporte.ColumnCount; j++)
+                        {
+                            worksheet.Cells[i + 10, j + 4] = dgvReporte.Rows[i].Cells[j].Value;
+                        }
+                    }
+                    SaveFileDialog dialogo = new SaveFileDialog();
+                    if (dialogo.ShowDialog() != DialogResult.OK)
+                    {
+                        return;
+                    }
+                    excelApp.ActiveWorkbook.SaveAs(dialogo.FileName);
+                    MessageBox.Show("Datos guardados correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filtro = comboBox1.SelectedItem.ToString();
+            if (!string.IsNullOrEmpty(filtro))
+            {
+                DataView dv = new DataView(dataTable); 
+                dv.RowFilter = "Año = '" + filtro + "'"; 
+                dgvReporte.DataSource = dv;
+                button1.Enabled = true;
+            }
+        }
+    }
+}
